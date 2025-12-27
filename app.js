6a6451fb6afda6f6e9d9a85724578e70257e5f2b
Soubhik Samanta <soubhiksamanta25@gmail.com>
2025-12-27 22:57:07 +0530

    Refactor app.js to render a parent component with child elements instead of a single heading

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
- const heading = React.createElement("h1",{id:"heading"},"Hello from my first React Code")
+//  const heading = React.createElement("h1",{id:"heading"},"Hello from my first React Code")
 
-      const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-      root.render(heading);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "tag" }, "hi i am a child 1 h1 tag"),
+    React.createElement("h2", { id: "tag" }, "hi i am the tag from child 1 h2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "tag" }, "hi i am h1 tag"),
+    React.createElement("h2", { id: "tag" }, "hi i am h2"),
+  ]),
+]);
+root.render(parent);
